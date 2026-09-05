c568eb85b1b28f1027254cbdf7a5fc433369b4a3
FlyingTechGuy <146762239+FlyingTechGuy@users.noreply.github.com>
2026-09-06 00:24:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { All_song } from './songsList.js';
+
 let previous = document.querySelector('#prevBtnBox');
 let play = document.querySelector('#playBtnBox');
 let next = document.querySelector('#nextBtnBox');
@@ -11,12 +13,14 @@ let auto_play = document.querySelector('#apBtnBox');
 let shuffle = document.querySelector('#shBtnBox');
 let karaoke = document.querySelector('#kBtnBox');
 let forward_five = document.querySelector('#fiveSecForward');
+let forward_five_btn = document.querySelector('#forwardFiveBtn');
 let backward_five = document.querySelector('#fiveSecBackward');
+let backward_five_btn = document.querySelector('#backwardFiveBtn');
 let cur_song_num = document.querySelector('#songNumCur');
 let tot_song_num = document.querySelector('#songNumTot');
 let m = document.querySelector('#m');
 let s = document.querySelector('#s');
-let total_time = document.querySelector('#totalTime');
+let totalTime = document.querySelector('#totalTime');
 let pl_box = document.querySelector('#plSongsBox');
 // let styleDir = document.querySelector('[data="dir"]');
 
@@ -72,314 +76,8 @@ track.addEventListener('play', () => {
     }
 });
 
-//All songs list
-let All_song = [
-  // Reggaton
-   [{
-     name: "AMANECE",
-     path: "music/Reggaeton/AnuelAA&HazeAMANECE.m4a",
-     img: "cover/Reggaeton/AnuelAA&HazeAMANECE.avif",
-     singer: "Anuel AA & Haze",
-     songtime: "3:11",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/anuelaa/amanece.html"
-   },
-   {
-     name: "Canción Bonita",
-     path: "music/Reggaeton/CarlosVivesRickyMartinCanciónBonita.m4a",
-     img: "cover/Reggaeton/CarlosVivesRickyMartinCanciónBonita.avif",
-     singer: "Carlos Vives, Ricky Martin",
-     songtime: "2:59",
-     karaoke: "",
-     lyrics: "https://genius.com/Carlos-vives-and-ricky-martin-cancion-bonita-lyrics"
-   },
-   {
-     name: "X ESO BB!",
-     path: "music/Reggaeton/JereKlein&NickiNicoleXEsoBB!Enfasis.m4a",
-     img: "cover/Reggaeton/JereKlein&NickiNicoleXEsoBB!Enfasis.avif",
-     singer: "JERE KLEIN & NICKI NICOLE",
-     songtime: "3:20",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/jereklein/xesobb.html"
-   },
-   {
-     name: "Mírame Ahora",
-     path: "music/Reggaeton/MírameAhora(SaludMiReina)MTZManuelTurizo.m4a",
-     img: "cover/Reggaeton/MírameAhora(SaludMiReina)MTZManuelTurizo.avif",
-     singer: "MTZ Manuel Turizo",
-     songtime: "2:34",
-     karaoke: "",
-     lyrics: "https://genius.com/Manuel-turizo-mirame-ahora-salud-mi-reina-lyrics"
-   },
-   {
-     name: "Soso Remix",
-     path: "music/Reggaeton/OmahLayXOzunaSosoRemixAFRO.m4a",
-     img: "cover/Reggaeton/ozunaAfro.avif",
-     singer: "Omah Lay X Ozuna",
-     songtime: "3:05",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/omahlay/sosoremix.html"
-   },
-   {
-     name: "Curarme El Alma",
-     path: "music/Reggaeton/OzunaCurarmeElAlmaAfro.m4a",
-     img: "cover/Reggaeton/ozunaAfro.avif",
-     singer: "Ozuna",
-     songtime: "2:00",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/ozuna/curarmeelalma.html"
-   },
-   {
-     name: "Isla Desierta",
-     path: "music/Reggaeton/OzunaIslaDesiertaAfro.m4a",
-     img: "cover/Reggaeton/ozunaAfro.avif",
-     singer: "Ozuna",
-     songtime: "2:56",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/ozuna/isladesierta.html"
-   },
-   {
-     name: "Una Perla En San Juan",
-     path: "music/Reggaeton/OzunaUnaPerlaEnSanJuanAfro.m4a",
-     img: "cover/Reggaeton/ozunaAfro.avif",
-     singer: "Ozuna",
-     songtime: "3:38",
-     karaoke: "",
-     lyrics: "https://www.azlyrics.com/lyrics/ozuna/unaperlaensanjuan.html"
-   }],
-
-   // Chill Vibes
-   [{
-    name: "Dynamite",
-    path: "songs/Dynamite.mp3",
-    img: "img/Dynamite.jpeg",
-    bgimg: "img/DynamiteBg.jpeg",
-    singer: "Taio Cruz",
-    songtime: "4:16",
-    karaoke: "songs/DynamiteK.mp3",
-    lyrics: "https://www.azlyrics.com/lyrics/taiocruz/dynamite.html"
-   },
-   {
-     name: "Soweto",
-     path: "songs/Soweto.mp3",
-     img: "img/Soweto.jpeg",
-     bgimg: "img/Sowetobg.webp",
-     singer: "Victony, Tempoe",
-     songtime: "?:??",
-     karaoke: "songs/SowetoK.mp3",
-     lyrics: "https://www.azlyrics.com/lyrics/victony/soweto.html"
-   },
-   {
-     name: "Mood",
-     path: "songs/Mood.mp3",
-     img: "img/Mood.jpeg",
-     bgimg: "img/Moodbg.jpeg",
-     singer: "24kGoldn",
-     songtime: "?:??",
-     karaoke: "songs/MoodK.mp3",
-     lyrics: "https://www.azlyrics.com/lyrics/24kgoldn/mood.html"
-   }/*,
-   {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }*/],
-
-   // Electronic/Dance
-   [{
-    name: "Alone",
-    path: "songs/Alone.mp3",
-    img: "img/Alone.jpeg",
-    bgimg: "img/AloneBg.jpeg",
-    singer: "Marshmello",
-    songtime: "?:??"
-  },
-  {
-    name: "Levels",
-    path: "songs/Levels.mp3",
-    img: "img/Levels.jpeg",
-    bgimg: "img/Levelsbg.webp",
-    singer: "Avichi",
-    songtime: "?:??"
-  },
-  {
-    name: "Peru",
-    path: "songs/Peru.mp3",
-    img: "img/Peru.jpeg",
-    bgimg: "img/Perubg.jpeg",
-    singer: "Fireboy DML, Ed Sheeran",
-    songtime: "?:??"
-  }/*,
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }*/],
-
-  // Pop
-  [{
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.webp",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }/*,
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }*/],
-
-  // Afrobeats
-  [{
-    name: "Commando",
-    path: "songs/Commando.mp3",
-    img: "img/Commando.jpeg",
-    bgimg: "img/CommandoBg.jpeg",
-    singer: "Mavokali",
-    songtime: "?:??"
-  },
-  {
-    name: "Calm Down",
-    path: "songs/CalmDown.mp3",
-    img: "img/CalmDown.jpeg",
-    bgimg: "img/CalmDownBg.jpeg",
-    singer: "Rema",
-    songtime: "?:??"
-  },
-  {
-    name: "KU LO SA",
-    path: "songs/KULOSA.mp3",
-    img: "img/KULOSA.jpeg",
-    bgimg: "img/KULOSABg.jpeg",
-    singer: "Oxlade, Camila Cabello",
-    songtime: "?:??"
-  },
-  {
-    name: "Am I Wrong",
-    path: "songs/AmIWrong.mp3",
-    img: "img/AmIWrong.jpeg",
-    bgimg: "img/AmIWrongBg.jpeg",
-    singer: "Nico, Vinz",
-    songtime: "?:??"
-  }/*,
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }*/],
-  
-  // Hip-Hop/Rap
-  [{
-    name: "Up Down",
-    path: "songs/UpDown.mp3",
-    img: "img/UpDown.jpeg",
-    bgimg: "img/UpDownBg.jpeg",
-    singer: "T-Pain",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.webp",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }/*,
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  },
-  {
-    name: "???",
-    path: "songs/.mp3",
-    img: "img/.jpeg",
-    bgimg: "img/Bg.jpeg",
-    singer: "???",
-    songtime: "?:??"
-  }*/]
-];
+// All songs list
+// * Moved to an external file *
 
 // All functions
 
@@ -393,7 +91,7 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener("keydown" , (event) => {
   if (event.key === 'a'){
     event.preventDefault();
-    autoplay_switch();
+    autoplay_toggle();
   }
 });
 
@@ -421,14 +119,14 @@ window.addEventListener("keydown" , (event) => {
 window.addEventListener("keydown" , (event) => {
   if (event.key === 'l'){
     event.preventDefault();
-    forward_five_sec();
+    forwardFive();
   }
 });
 
 window.addEventListener("keydown" , (event) => {
   if (event.key === 'j'){
     event.preventDefault();
-    backward_five_sec();
+    backwardFive();
   }
 });
 
@@ -443,6 +141,14 @@ window.addEventListener("keydown" , (event) => {
   if (event.key === 'p'){
     event.preventDefault();
     previous_song();
+  }
+});
+
+window.addEventListener("keydown" , (event) => {
+  if (event.shiftKey && event.key === 'P'){
+    event.preventDefault();
+    document.getElementById("plCont").classList.toggle("show");
+    document.getElementById("plBox").classList.toggle("show");
   }
 });
 
@@ -586,7 +292,7 @@ function previous_song() {
     updatePl();
 }
 
-function nextTitleShow(x) {
+function nextTitleShow() {
     let i;
     if (index_no < All_song[pl].length - 1) {
       i = index_no + 1;
@@ -594,30 +300,30 @@ function nextTitleShow(x) {
     else {
       i = 0;
     }
-    x.title = All_song[pl][i].name + " - " + All_song[pl][i].singer;
+    next.title = All_song[pl][i].name + " - " + All_song[pl][i].singer;
 }
 
-function previousTitleShow(x) {
+function previousTitleShow() {
     let i;
     if (index_no > 0) {
-		i = index_no - 1;
-	}
+		  i = index_no - 1;
+	  }
     else {
-		i = All_song[pl].length - 1;
-	}
-    x.title = All_song[pl][i].name + " - " + All_song[pl][i].singer;
+		  i = All_song[pl].length - 1;
+	  }
+    previous.title = All_song[pl][i].name + " - " + All_song[pl][i].singer;
 }
 
-function playPauseTitle(x) {
+function playPauseTitle() {
     if (Playing_song) {
-        x.title = "Pause";
+        play.title = "Pause";
     }
     else {
-        x.title = "Play";
+        play.title = "Play";
     }
 }
 
-function load_track(index_no){
+function load_track(index_no) {
     clearInterval(timer);
 	  reset_slider();
 	  track.src = All_song[pl][index_no].path;
@@ -627,6 +333,12 @@ function load_track(index_no){
     artist.innerHTML = All_song[pl][index_no].singer;
     // totalTime.innerHTML = All_song[pl][index_no].songtime;
     track.load();
+    let time = Math.floor(track.currentTime);
+    let minutes = Math.floor(time/60);
+    let secends = time%60;
+    secends = String(secends).padStart(2, '0');
+    m.innerHTML = minutes;
+    s.innerHTML = secends;
 	  timer = setInterval(range_slider ,1000);
 	  tot_song_num.innerHTML = All_song[pl].length;
 	  cur_song_num.innerHTML = index_no + 1;
@@ -644,16 +356,16 @@ track.addEventListener('loadedmetadata', () => {
 load_track(index_no);
 
 // change slider position 
-function change_duration(){
-	// slider_position = track.duration * (slider.value / 100);
-	// track.currentTime = slider_position;
-    // let time = Math.floor(track.currentTime);
-    // let minutes = Math.floor(time/60);
-    // let secends = time%60;
-    // secends = secends < 10 ? '0' + secends : secends;
-    // m.innerHTML = minutes;
-    // s.innerHTML = secends;
-}
+// function change_duration() {
+// 	slider_position = track.duration * (slider.value / 100);
+// 	track.currentTime = slider_position;
+//   let time = Math.floor(track.currentTime);
+//   let minutes = Math.floor(time/60);
+//   let secends = time%60;
+//   secends = secends < 10 ? '0' + secends : secends;
+//   m.innerHTML = minutes;
+//   s.innerHTML = secends;
+// }
 
 // autoplay function
 function autoplay_toggle() {
@@ -770,6 +482,20 @@ function backwardFive() {
     }
 }
 
+karaoke.addEventListener("click", karaoke_toggle);
+shuffle.addEventListener("click", shuffle_toggle);
+previous.addEventListener("click", previous_song);
+previous.addEventListener("mouseover", previousTitleShow);
+play.addEventListener("click", justplay);
+previous.addEventListener("mouseover", playPauseTitle);
+next.addEventListener("click", next_song);
+previous.addEventListener("mouseover", nextTitleShow);
+auto_play.addEventListener("click", autoplay_toggle);
+// slider.addEventListener("change", change_duration);
+forward_five_btn.addEventListener("click", forwardFive);
+backward_five_btn.addEventListener("click", backwardFive);
+
+let slider_position;
 document.addEventListener("DOMContentLoaded", function() {
     const duration_slider = document.getElementById("duration_slider");
     duration_slider.addEventListener("input", function() {
@@ -782,18 +508,21 @@ document.addEventListener("DOMContentLoaded", function() {
         m.innerHTML = minutes;
         s.innerHTML = secends;
     });
+    let plSongsOverlay = document.querySelectorAll(".plSongOverlay");
+    plSongsOverlay.forEach(plSongsButton => {
+      plSongsButton.addEventListener('click', (event) => {
+          selectSong(Number(event.target.dataset.id));
+          console.log(event.target.dataset.id);
+      });
+    });
 });
 
-function range_slider(){
-	  let position = 0;
-        
+let position = 0;
+function range_slider() {
     // update slider position
-	  if(!isNaN(track.duration)){
+	  if(!isNaN(track.duration)) {
 	    position = track.currentTime * (100 / track.duration);
 	    slider.value =  position;
-	  }
-
-    if(!isNaN(track.duration)){
       let time = Math.floor(track.currentTime);
       let minutes = Math.floor(time/60);
       let secends = time%60;
@@ -863,7 +592,8 @@ function setPl() {
   pl_box.innerHTML = "";
   for (let i = 0; i < All_song[pl].length; i++) {
     newSong = `
-    <div class="plSong" onclick="selectSong(${i})">
+    <div class="plSong">
+        <div class="plSongOverlay" data-id="${i}"></div>
         <p class="plSongNum">${i+1}</p>
         <div class="plSongRight">
             <div class="plSongInfo">
---
+++ songsList.js
@@ -0,0 +1,307 @@
+export let All_song = [
+  // Reggaton
+   [{
+     name: "AMANECE",
+     path: "music/Reggaeton/AnuelAA&HazeAMANECE.m4a",
+     img: "cover/Reggaeton/AnuelAA&HazeAMANECE.avif",
+     singer: "Anuel AA & Haze",
+     songtime: "3:11",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/anuelaa/amanece.html"
+   },
+   {
+     name: "Canción Bonita",
+     path: "music/Reggaeton/CarlosVivesRickyMartinCanciónBonita.m4a",
+     img: "cover/Reggaeton/CarlosVivesRickyMartinCanciónBonita.avif",
+     singer: "Carlos Vives, Ricky Martin",
+     songtime: "2:59",
+     karaoke: "",
+     lyrics: "https://genius.com/Carlos-vives-and-ricky-martin-cancion-bonita-lyrics"
+   },
+   {
+     name: "X ESO BB!",
+     path: "music/Reggaeton/JereKlein&NickiNicoleXEsoBB!Enfasis.m4a",
+     img: "cover/Reggaeton/JereKlein&NickiNicoleXEsoBB!Enfasis.avif",
+     singer: "JERE KLEIN & NICKI NICOLE",
+     songtime: "3:20",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/jereklein/xesobb.html"
+   },
+   {
+     name: "Mírame Ahora",
+     path: "music/Reggaeton/MírameAhora(SaludMiReina)MTZManuelTurizo.m4a",
+     img: "cover/Reggaeton/MírameAhora(SaludMiReina)MTZManuelTurizo.avif",
+     singer: "MTZ Manuel Turizo",
+     songtime: "2:34",
+     karaoke: "",
+     lyrics: "https://genius.com/Manuel-turizo-mirame-ahora-salud-mi-reina-lyrics"
+   },
+   {
+     name: "Soso Remix",
+     path: "music/Reggaeton/OmahLayXOzunaSosoRemixAFRO.m4a",
+     img: "cover/Reggaeton/ozunaAfro.avif",
+     singer: "Omah Lay X Ozuna",
+     songtime: "3:05",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/omahlay/sosoremix.html"
+   },
+   {
+     name: "Curarme El Alma",
+     path: "music/Reggaeton/OzunaCurarmeElAlmaAfro.m4a",
+     img: "cover/Reggaeton/ozunaAfro.avif",
+     singer: "Ozuna",
+     songtime: "2:00",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/ozuna/curarmeelalma.html"
+   },
+   {
+     name: "Isla Desierta",
+     path: "music/Reggaeton/OzunaIslaDesiertaAfro.m4a",
+     img: "cover/Reggaeton/ozunaAfro.avif",
+     singer: "Ozuna",
+     songtime: "2:56",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/ozuna/isladesierta.html"
+   },
+   {
+     name: "Una Perla En San Juan",
+     path: "music/Reggaeton/OzunaUnaPerlaEnSanJuanAfro.m4a",
+     img: "cover/Reggaeton/ozunaAfro.avif",
+     singer: "Ozuna",
+     songtime: "3:38",
+     karaoke: "",
+     lyrics: "https://www.azlyrics.com/lyrics/ozuna/unaperlaensanjuan.html"
+   }],
+
+   // Chill Vibes
+   [{
+    name: "Dynamite",
+    path: "songs/Dynamite.mp3",
+    img: "img/Dynamite.jpeg",
+    bgimg: "img/DynamiteBg.jpeg",
+    singer: "Taio Cruz",
+    songtime: "4:16",
+    karaoke: "songs/DynamiteK.mp3",
+    lyrics: "https://www.azlyrics.com/lyrics/taiocruz/dynamite.html"
+   },
+   {
+     name: "Soweto",
+     path: "songs/Soweto.mp3",
+     img: "img/Soweto.jpeg",
+     bgimg: "img/Sowetobg.webp",
+     singer: "Victony, Tempoe",
+     songtime: "?:??",
+     karaoke: "songs/SowetoK.mp3",
+     lyrics: "https://www.azlyrics.com/lyrics/victony/soweto.html"
+   },
+   {
+     name: "Mood",
+     path: "songs/Mood.mp3",
+     img: "img/Mood.jpeg",
+     bgimg: "img/Moodbg.jpeg",
+     singer: "24kGoldn",
+     songtime: "?:??",
+     karaoke: "songs/MoodK.mp3",
+     lyrics: "https://www.azlyrics.com/lyrics/24kgoldn/mood.html"
+   }/*,
+   {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }*/],
+
+   // Electronic/Dance
+   [{
+    name: "Alone",
+    path: "songs/Alone.mp3",
+    img: "img/Alone.jpeg",
+    bgimg: "img/AloneBg.jpeg",
+    singer: "Marshmello",
+    songtime: "?:??"
+  },
+  {
+    name: "Levels",
+    path: "songs/Levels.mp3",
+    img: "img/Levels.jpeg",
+    bgimg: "img/Levelsbg.webp",
+    singer: "Avichi",
+    songtime: "?:??"
+  },
+  {
+    name: "Peru",
+    path: "songs/Peru.mp3",
+    img: "img/Peru.jpeg",
+    bgimg: "img/Perubg.jpeg",
+    singer: "Fireboy DML, Ed Sheeran",
+    songtime: "?:??"
+  }/*,
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }*/],
+
+  // Pop
+  [{
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.webp",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }/*,
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }*/],
+
+  // Afrobeats
+  [{
+    name: "Commando",
+    path: "songs/Commando.mp3",
+    img: "img/Commando.jpeg",
+    bgimg: "img/CommandoBg.jpeg",
+    singer: "Mavokali",
+    songtime: "?:??"
+  },
+  {
+    name: "Calm Down",
+    path: "songs/CalmDown.mp3",
+    img: "img/CalmDown.jpeg",
+    bgimg: "img/CalmDownBg.jpeg",
+    singer: "Rema",
+    songtime: "?:??"
+  },
+  {
+    name: "KU LO SA",
+    path: "songs/KULOSA.mp3",
+    img: "img/KULOSA.jpeg",
+    bgimg: "img/KULOSABg.jpeg",
+    singer: "Oxlade, Camila Cabello",
+    songtime: "?:??"
+  },
+  {
+    name: "Am I Wrong",
+    path: "songs/AmIWrong.mp3",
+    img: "img/AmIWrong.jpeg",
+    bgimg: "img/AmIWrongBg.jpeg",
+    singer: "Nico, Vinz",
+    songtime: "?:??"
+  }/*,
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }*/],
+  
+  // Hip-Hop/Rap
+  [{
+    name: "Up Down",
+    path: "songs/UpDown.mp3",
+    img: "img/UpDown.jpeg",
+    bgimg: "img/UpDownBg.jpeg",
+    singer: "T-Pain",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.webp",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }/*,
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  },
+  {
+    name: "???",
+    path: "songs/.mp3",
+    img: "img/.jpeg",
+    bgimg: "img/Bg.jpeg",
+    singer: "???",
+    songtime: "?:??"
+  }*/]
+];
